0489c8b351a59f6aaa9d995e1d6b34da63b6e13e
Halil İbrahim Demir <hibrahimd5@gmail.com>
2023-06-23 12:18:34 +0300

    test ready

--- test/counter_cubit/counter_cubit_test.dart
+++ test/counter_cubit/counter_cubit_test.dart
@@ -1,15 +1,29 @@
 import 'package:bloc_git/counter_cubit/counter_cubit.dart';
 import 'package:bloc_git/counter_cubit/counter_state.dart';
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('CounterCubit', () {
-    late final CounterCubit counterCubit;
+      CounterCubit? counterCubit;
     setUp(() => counterCubit = CounterCubit());
-    tearDown(() => counterCubit.close());
-    test('başlangıç durumu ile verilen durum aynıdır', () {
-      expect(counterCubit.state,
+    tearDown(() => counterCubit!.close());
+
+    test('', () {
+      expect(counterCubit!.state,
           const CounterState(countValue: 0, wasIncremented: false));
     });
+    blocTest(
+      'increment is succefull.',
+      build: () => counterCubit!,
+      act: (cubit) => cubit.increment(),
+      expect: () => const [CounterState(countValue: 1, wasIncremented: true)],
+    );
+    blocTest(
+      'decrement is succefull.',
+      build: () => counterCubit!,
+      act: (cubit) => cubit.decrement(),
+      expect: () => const [CounterState(countValue: -1, wasIncremented: false)],
+    );
   });
 }
